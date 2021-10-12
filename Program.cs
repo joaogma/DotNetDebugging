c0aabb88f6bf04a671d87479310a753d0d9ef0cc
João Amendola <mx.joaoo@gmail.com>
2021-10-12 00:53:54 -0300

    Added Debugs to track sum, n1 and n2 inside of the for loop

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace DotNetDebugging
 {
@@ -12,6 +13,8 @@ namespace DotNetDebugging
         }
         static int Fibonacci(int n)
         {
+            Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+            Debug.WriteLine($"We are looking for the {n}th number");
             int n1 = 0;
             int n2 = 1;
             int sum = 0;
@@ -21,6 +24,7 @@ namespace DotNetDebugging
                 sum = n1 + n2;
                 n1 = n2;
                 n2 = sum;
+                Debug.WriteLineIf(sum == 1, $"sum is {sum}, n1 is {n1}, n2 is {n2}");
             }
             return n == 0 ? n1 : n2;
         }
